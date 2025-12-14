8abd2466d9e532b322fdd85e37eba71b565571d1
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-12-14 18:24:19 +0630

    update

---
+++ Animal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace tuto_01
+{
+    internal class Animal
+    {
+
+        string type = "Dog";
+        static int age = 10;
+        string name;
+
+        public void Run(string type)
+        {
+            this.type = type;
+            Console.WriteLine(this.type + " running ...");
+            Animal.Name();
+            age++;
+            Animal.age++;
+            Console.WriteLine(Animal.age);
+
+        }
+
+        static void Name()
+        {
+            var name = " shwe war";
+            Animal a = new ();
+            a.name = name;
+            Console.WriteLine(a.name);
+            Console.WriteLine(a.type);
+            Console.WriteLine(Animal.age);
+        }
+
+        public string Eat()
+        {
+            return "eating ..." + this.type;
+        }
+
+        public static string Sleep()
+        {
+            return "Sleep ... ";
+        }
+
+
+    }
+}
